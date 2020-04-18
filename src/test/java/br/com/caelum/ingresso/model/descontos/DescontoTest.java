a76aeec1aacb9f8de9a3312d40372d26ea4d3bf7
Mauricio Viana <mau_viana@live.com>
2020-04-18 12:01:07 -0300

    selecionar cadeiras disponiveis

--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -10,39 +10,48 @@ import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 
 public class DescontoTest {
 	private Sala sala;
 	private Filme filme;
 	private Sessao sessao; 
+	private Lugar lugar;
+	private Ingresso ingressoSemDesconto;
+	private Ingresso ingressoDescontoBanco;
+	private Ingresso ingressoDescontoEstudante;
 	
 	@Before
 	public void preparaSessoes() {
 		this.sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
 		this.filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
 		this.sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		this.lugar = new Lugar("A", 1);
+		
+		this.ingressoDescontoBanco = new Ingresso(sessao, TipoDeIngresso.BANCO, lugar);
+		this.ingressoDescontoEstudante = new Ingresso(sessao, TipoDeIngresso.ESTUDANTE, lugar);
+		this.ingressoSemDesconto = new Ingresso(sessao, TipoDeIngresso.INTEIRO, lugar);
 	}
 	
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal() {
-		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
-		BigDecimal precoEsperado = new BigDecimal("32.50");	
-		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+		BigDecimal precoEsperado = new BigDecimal("32.50");
+		Assert.assertEquals(precoEsperado, ingressoSemDesconto.getPreco());
 	}
 
 	@Test
 	public void deveConcederDescontoDe30PorcentoParaIngressosDeClientesDeBancos() {
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
 		BigDecimal precoEsperado = new BigDecimal("22.75");
-		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+		Assert.assertEquals(precoEsperado, ingressoDescontoBanco.getPreco());
 	}
 
 	@Test
 	public void deveConcederDescontoDe50PorcentoParaIngressoDeEstudante() {
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		
 		BigDecimal precoEsperado = new BigDecimal("16.25");
-		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+		Assert.assertEquals(precoEsperado, ingressoDescontoEstudante.getPreco());
 	}
 }
