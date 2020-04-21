44f3f858309e8d5757977eb91625984d33017d35
Mauricio Viana <mau_viana@live.com>
2020-04-21 12:33:17 -0300

    enviando e-mail

--- src/main/java/br/com/caelum/ingresso/controller/UsuarioController.java
+++ src/main/java/br/com/caelum/ingresso/controller/UsuarioController.java
@@ -1,12 +1,28 @@
 package br.com.caelum.ingresso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.caelum.ingresso.email.EmailNovoUsuario;
+import br.com.caelum.ingresso.email.Mailer;
+import br.com.caelum.ingresso.email.Token;
+import br.com.caelum.ingresso.helper.TokenHelper;
+
 @Controller
 public class UsuarioController {
+
+	private Mailer mailer;
+	private TokenHelper tokenHelper;
+
+	@Autowired
+	public UsuarioController(Mailer mailer, TokenHelper tokenHelper) {
+		this.mailer = mailer;
+		this.tokenHelper = tokenHelper;
+	}
 
 	@GetMapping
 	private ModelAndView formSolicitacaoDeAcesso() {
@@ -14,7 +30,11 @@ public class UsuarioController {
 	}
 
 	@PostMapping("/usuario/request")
+	@Transactional
 	public ModelAndView solicitacaoDeAcesso(String email) {
-		return new ModelAndView("usuario/adicionado");
+		ModelAndView view = new ModelAndView("usuario/adicionado");
+		Token token = tokenHelper.generateFrom(email);
+		mailer.send(new EmailNovoUsuario(token));
+		return view;
 	}
 }
